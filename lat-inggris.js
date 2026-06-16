6e7b5a9c27fe0ddef7fc6e728db430abff0ba399
mudamudisedahromolor <mudamudisedahromolor@gmail.com>
2026-06-16 18:29:55 +0100

    Update lat-inggris.js

--- lat-inggris.js
+++ lat-inggris.js
@@ -717,10 +717,7 @@ function tampilkanMateriSpesifik(namaMateriKolomC, subMateriKolomD) {
                                     <span style="font-size:12.5px; color:#71717a;"><i>Artinya: Dia <b>tidak hanya</b> pintar, <b>tetapi juga</b> baik hati.</i></span>
                                 </td>
                             </tr>
-                            <tr style="background:#fefce8;">
-                                <td style="padding:10px; color:#a1a1aa; font-style:italic; font-weight:600; border-right:1px solid #cbd5e1;">etc.</td>
-                                <td style="padding:10px; color:#a1a1aa; font-size:12px; font-style:italic;">Selengkapnya lihat pada menu Ensiklopedia</td>
-                            </tr>
+                            
                         </tbody>
                     </table>
                 </div>
