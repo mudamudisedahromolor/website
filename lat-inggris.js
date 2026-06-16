09284524b49ed64805f1a661a47e8fa24e4a3662
mudamudisedahromolor <mudamudisedahromolor@gmail.com>
2026-06-16 17:21:34 +0100

    Update lat-inggris.js

--- lat-inggris.js
+++ lat-inggris.js
@@ -331,10 +331,7 @@ function tampilkanMateriSpesifik(namaMateriKolomC, subMateriKolomD) {
                                     <span style="color:#2563eb; font-weight:600; font-style:italic;">Contoh:</span> "She is studying in <span style="color:#0f172a; font-weight:700;">the library</span>."
                                 </td>
                             </tr>
-                            <tr style="background:#fefce8;">
-                                <td style="padding:10px; color:#a1a1aa; font-style:italic; font-weight:600; border-right:1px solid #cbd5e1;">etc.</td>
-                                <td style="padding:10px; color:#a1a1aa; font-size:12px; font-style:italic;">Selengkapnya lihat pada menu Ensiklopedia</td>
-                            </tr>
+                            
                         </tbody>
                     </table>
                 </div>
