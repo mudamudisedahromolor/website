40c3e740e42cecc56d12e4f886af5b2c71787b84
mudamudisedahromolor <mudamudisedahromolor@gmail.com>
2026-06-17 11:25:24 +0100

    Update Button.js

--- bilyard/script/menu/Button.js
+++ bilyard/script/menu/Button.js
@@ -1,5 +1,6 @@
-function Button(sprite, position, callback, hoverSprite){
+"use strict";
 
+function Button(sprite, position, callback, hoverSprite){
     this.sprite = sprite;
     this.hoverSprite = hoverSprite ? hoverSprite : sprite;
     this.position = position;
@@ -7,37 +8,43 @@ function Button(sprite, position, callback, hoverSprite){
 }
 
 Button.prototype.draw = function(){
-
     if(this.mouseInsideBorders()){
         Canvas2D.drawImage(this.hoverSprite, this.position, 0, 1);
-        Canvas2D._canvas.style.cursor = "pointer";
+
+        if (Canvas2D && Canvas2D._canvas) {
+            Canvas2D._canvas.style.cursor = "pointer";
+        }
     }
     else{
         Canvas2D.drawImage(this.sprite, this.position, 0, 0.98);
     }
-}
+};
 
 Button.prototype.handleInput = function(){
-
-    if(Mouse.left.pressed && this.mouseInsideBorders()){
+    if((Mouse.left.pressed || Mouse.left.down) && this.mouseInsideBorders()){
         this.callback();
+
+        // Supaya satu tap tidak memanggil callback berkali-kali.
+        Mouse.reset();
+        Mouse.left.down = false;
     }
-}
+};
 
 Button.prototype.mouseInsideBorders = function(){
-    
-    mousePos = Mouse.position;
+    var mousePos = Mouse.position;
 
-    if(mousePos.x > this.position.x 
-        &&
-        mousePos.x < this.position.x + this.sprite.width
-        &&
-        mousePos.y > this.position.y
-        &&
+    if(!this.sprite) {
+        return false;
+    }
+
+    if(
+        mousePos.x > this.position.x &&
+        mousePos.x < this.position.x + this.sprite.width &&
+        mousePos.y > this.position.y &&
         mousePos.y < this.position.y + this.sprite.height
     ){
         return true;
     }
 
     return false;
-}
+};
